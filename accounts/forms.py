d175b4c6b1bc07e089c243f45041a583a4c02b63
Ikenna <hnikenna@yahoo.com>
2022-07-04 12:49:01 +0100

    Second Official Commit!

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email",)
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ("first_name", "last_name", "username", "phone",)
